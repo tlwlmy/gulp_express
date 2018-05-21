00913de42242a47c347f90fd201e52e0e652e263
tlwlmy <tlwlmy@github.com>
2018-05-21 21:13:43 +0800

    修改使用页面

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var functions = require('./../common/functions')
-//var aes_cipher = require('./../module/aes-cipher')
 
 //index page
 router.get('/sv',function(req, res){
   // 生产获取素材链接
   murl = functions.generate_material_url()
-  console.log(murl)
 
   // 生产分享链接
   //surl = functions.genereate_share_url(req.headers['host'])
   surl = functions.generate_share_url('wx.sysecq.com')
 
-  res.render('v3A',{
+  res.render('fission',{
     murl: murl,
     surl: surl
   })
